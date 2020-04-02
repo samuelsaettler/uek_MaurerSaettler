e1dfdc55582ac4d02377568345f847e089f6c38e
Samuel Sättler <samuel.saettler@edu.tbz.ch>
2020-04-02 17:56:28 +0200

    delete false file

--- Exercise_4/uk/src/main/java/ch/noseryoung/uk/domainModels/article/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.noseryoung.uk.domainModels.article;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticleRepository extends JpaRepository<Article, Integer>{
-
-    public Article findByName(String name);
-}
